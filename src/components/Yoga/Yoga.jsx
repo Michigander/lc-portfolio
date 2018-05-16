3b76b112df496e8ac5d6786b1f406af04ef03ac6
Gordon Finnie <gordon.m.finnie.iii@gmail.com>
2018-05-16 18:49:46 -0400

    + yoga caption update

--- src/components/Yoga/Yoga.jsx
+++ src/components/Yoga/Yoga.jsx
@@ -6,7 +6,7 @@ const Yoga = () =>
   <div className="Yoga">
     <div className="Yoga__overlay" />
     <div className="Yoga__card">
-        I first began taking yoga classes in high school at Frog Lotus Yoga in North Adams, MA. My passion for yoga evolved over a decade of practice. I remain a devoted follower of the Frog Lotus School, recently completing my 200 hour CYT certificate in Thailand, under the incredible instruction of Jennifer Yarro. 
+        I first began taking yoga classes in high school at Frog Lotus Yoga in North Adams, MA. My passion for yoga evolved over a decade of practice. I remain a devoted follower of the Frog Lotus Yoga School, recently completing my 200 hour CYT certificate in Thailand, under the incredible instruction of Jennifer Yarro. 
       <br />
         I am grateful for the playful and creative, yet intelligent & mindful style of yoga that my many wonderful instructors instilled in my practice. I aims to bring this sense of playfulness, creativity, intelligence, & mindfulness to every class I instruct.  
     </div>
